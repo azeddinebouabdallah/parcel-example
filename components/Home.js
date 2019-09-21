e252119316f847fd5d4998bac9e1126b574a6e88
didoudesigner <bouabdallahazeddine@gmail.com>
2019-09-21 14:37:55 +0100

    Initial commit

---
+++ components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Home.css'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div className="home-page">
+                <h1>React.js with parceljs</h1>
+            </div>
+        )
+    }
+}
